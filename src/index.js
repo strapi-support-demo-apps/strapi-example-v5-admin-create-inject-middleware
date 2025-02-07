d07a35f7e862aea7eb5a90e34039a9235565212c
Derrick Mehaffy <derrickmehaffy@gmail.com>
2025-02-07 14:36:34 -0800

    move logic to a different file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const registerAdminProfile = require("./utils/registerAdminProfile");
+
 module.exports = {
   /**
    * An asynchronous register function that runs before
@@ -8,26 +10,7 @@ module.exports = {
    * This gives you an opportunity to extend code.
    */
   register(/*{ strapi }*/) {
-    const adminRoutes = strapi.admin.routes.admin.routes;
-
-    const adminProfileMiddleware = "global::admin-profile";
-
-    const indexID = adminRoutes.findIndex(
-      (route) => route.path === "/users" && route.method === "POST"
-    );
-
-    // instead of calling the middleware when the user is created you could do it when they configure their password
-    // const indexID = adminRoutes.findIndex(
-    //   (route) => route.path === "/register" && route.method === "POST"
-    // );
-
-    if (indexID !== -1) {
-      if (!adminRoutes[indexID].config.middlewares) {
-        adminRoutes[indexID].config.middlewares = [adminProfileMiddleware];
-      } else {
-        adminRoutes[indexID].config.middlewares.push(adminProfileMiddleware);
-      }
-    }
+    registerAdminProfile();
   },
 
   /**
